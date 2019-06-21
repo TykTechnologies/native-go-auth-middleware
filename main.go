785cf063fca014f4de22a800824ed833a220fa6a
Sedky Abou-Shamalah <sedkyaboushamalah@gmail.com>
2019-06-21 00:38:24 -0400

    Does actual auth check with base64 encoded basic-auth against dynamodb

--- main.go
+++ main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"encoding/base64"
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 )
 
 func main() {}
@@ -19,6 +23,7 @@ func DynamoDBAuth(w http.ResponseWriter, r *http.Request) {
 	})
 	if err != nil {
 		fmt.Println("Couldn't get AWS access")
+		returnNoAuth(w, "Internal Error")
 		return
 	}
 
@@ -26,10 +31,11 @@ func DynamoDBAuth(w http.ResponseWriter, r *http.Request) {
 	svc := dynamodb.New(sess)
 	if svc == nil {
 		fmt.Println("Couldn't create new DynamoDB session")
+		returnNoAuth(w, "Internal Error")
 		return
 	}
 
-	username := "foo" // read this from outside
+	username, password := unmarshalBasicAuth(r.Header.Get("Authorization"))
 
 	result, err := svc.GetItem(&dynamodb.GetItemInput{
 		TableName: aws.String("basic-auth"),
@@ -41,13 +47,49 @@ func DynamoDBAuth(w http.ResponseWriter, r *http.Request) {
 	})
 	if err != nil {
 		fmt.Println(err.Error())
+		returnNoAuth(w, "Internal Error")
+		return
+	} else if result.Item == nil {
+		returnNoAuth(w, "Username not found.")
+		return
+	}
+
+	basicAuth := BasicAuth{}
+	err = dynamodbattribute.UnmarshalMap(result.Item, &basicAuth)
+	if err != nil {
+		panic(fmt.Sprintf("Failed to unmarshal Record, %v", err))
+	}
+
+	// Check Password
+	if password != basicAuth.Hash {
+		returnNoAuth(w, "Wrong Password.")
 		return
 	}
 
 	fmt.Println(result)
 }
 
+func returnNoAuth(w http.ResponseWriter, errorMessage string) {
+	jsonData, err := json.Marshal(errorMessage)
+	if err != nil {
+		fmt.Println("Couldn't marshal")
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Write(jsonData)
+}
+
+func unmarshalBasicAuth(s string) (string, string) {
+	decoded, err := base64.StdEncoding.DecodeString(s)
+	if err != nil {
+		fmt.Println("decode error:", err)
+		panic("not decodable")
+	}
+	splitStr := strings.Split(string(decoded), ":")
+	return string(splitStr[0]), string(splitStr[1])
+}
+
 type BasicAuth struct {
 	Username string
-	Password string
+	Hash     string
 }
